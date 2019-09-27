98494cca2cf2d3817be6667d911e15d3a4788ce0
Jason <jikurata@gmail.com>
2019-09-26 21:27:59 -0700

    add project files

---
+++ src/Rune.js
@@ -0,0 +1,11 @@
+'use strict';
+const EventEmitter = require('events');
+
+class Rune extends EventEmitter {
+  constructor() {
+    super();
+
+  }
+}
+
+module.exports = Rune;
---
+++ test/runesmith.test.js
@@ -0,0 +1,13 @@
+'use strict';
+const Taste = require('@jikurata/taste');
+const Runesmith = require('../src/Runesmith.js');
+const Rune = require('../src/Rune.js');
+
+Taste.flavor('Expanding the Runesmith vocabulary')
+.describe('Declare a new Rune')
+.test(profile => {
+
+})
+.expect()
+
+Taste.flavor('')
